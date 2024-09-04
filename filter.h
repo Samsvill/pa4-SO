91fe60574fc0ddcae8e6ab93b4ec9e79f835bf98
Sam Sánchez <samsvill@espol.edu.ec>
2024-09-04 16:04:39 -0500

    Update filter.h

--- filter.h
+++ filter.h
@@ -1,15 +1,7 @@
-#include "bmp.h"
+#ifndef FILTER_H
+#define FILTER_H
 
-typedef struct {
-	BMP_Image * imageIn;
-	BMP_Image * imageOut;
-	int (*filter)[3];
-	int tid;
-	int numThreads;
-    float norm;
-	int startRow;
-	int endRow;
-} FilterThreadArgs;
+#include "bmp.h"
 
 // Declaraciones de filtros predefinidos
 extern int edgeEnhanceFilter[3][3];
@@ -17,3 +9,14 @@ extern int blurFilter[3][3];
 
 // Función para aplicar un filtro específico en un rango de filas
 void *applyFilter(void *args);
+
+// Estructura para pasar argumentos a los hilos
+typedef struct {
+    int startRow;
+    int endRow;
+    BMP_Image *imageIn;
+    BMP_Image *imageOut;
+    int filter[3][3];  // Permitir aplicar filtros diferentes
+} ThreadArgs;
+
+#endif // FILTER_H
